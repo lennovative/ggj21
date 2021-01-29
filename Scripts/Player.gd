11f0c1a99008e636f4cb841d94bea4995293b7d7
Lennart <mailtolennart@gmx.net>
2021-01-29 22:57:52 +0100

    better movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,10 +5,10 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 var vel = Vector2()
-var gravity = 800
+var gravity = 1200
 var walkspeed = 400
-var jumpSpeed = 700
-var acc = 1200
+var jumpSpeed = 600
+var acc = 1700
 
 
 # Called when the node enters the scene tree for the first time.
@@ -27,13 +27,10 @@ func get_input():
 func _physics_process(delta):
 	var target_speed = get_input() * walkspeed
 	vel.x = move_toward(vel.x, target_speed, delta * acc)# * abs(vel.x - target_speed) / walkspeed)
-	if Input.is_action_pressed("jump_button") and is_on_floor():
-		vel.y -= jumpSpeed
-	if Input.is_action_pressed("down_button"):
-		if is_on_floor():
+	if is_on_floor():
+		if Input.is_action_pressed("jump_button"):
+			vel.y -= jumpSpeed
+		if Input.is_action_pressed("down_button"):
 			self.position.y += 10
-		else:
-			vel.y += jumpSpeed / 4
-
 	vel.y += gravity * delta
 	vel = move_and_slide(vel, Vector2(0, -1))
