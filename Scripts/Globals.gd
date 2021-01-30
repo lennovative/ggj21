8fca0ddb809c3df4235453e908e51b2ddf4735e6
Sascha <stu217930@mail.uni-kiel.de>
2021-01-30 18:05:23 +0100

    added some item-effects

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,3 +1,4 @@
 extends Node
 
 const GRAVITY = 1200
+var light_level = Color.black
--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const GRAVITY = 1200
 
+onready var ambient = get_node("CanvasModulate")
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,5 +18,11 @@ func _ready():
 	an_item.set_position(Vector2(500, 1100))
 	self.add_child(an_item)
 	
+	var yarn_item = preload("res://Scenes/Item.tscn").instance()
+	yarn_item.init("yarn")
+	yarn_item.set_position(Vector2(300, 1100))
+	self.add_child(yarn_item)
 	
+func _process(delta):
+	ambient.set_color(Globals.light_level)
 
