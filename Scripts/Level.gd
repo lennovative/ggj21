e917267ba98129166320839fd12e2c6536f41c44
Felan7 <50790924+Felan7@users.noreply.github.com>
2021-01-30 22:16:08 +0100

    Added Items

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -16,13 +16,32 @@ func spawnItem(type:String, position: Vector2):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawnItem("book_blue", Vector2(-1250, 1050))
+	spawnItem("book_red", Vector2(1250, 1050))
+	spawnItem("book_green", Vector2(1500, 1050))
 	spawnItem("cat", Vector2(1775, 750))
-	spawnItem("radio", Vector2(-2600, 500))
-	objective = spawnItem("glasses", Vector2(-1300, 800))
+	spawnItem("chest", Vector2(-1500, 800))
 	spawnItem("coffee", Vector2(1050, 1050))
-	spawnItem("yarn", Vector2(300, 1100))
+	#spawnItem("creeper_plush", Vector2())
+	spawnItem("doll", Vector2(4250, 800))
 	spawnItem("echolocator", Vector2(-4900, 1050))
+	spawnItem("flour", Vector2(-1250, 1050))
+	objective = spawnItem("glasses", Vector2(-1300, 800))
+	spawnItem("ice_cubes", Vector2(4500, 1050))
+	spawnItem("keys", Vector2(4250, 1050))
+	spawnItem("yarn", Vector2(750, 1100))
 	spawnItem("tv", Vector2(3750, 1050))
+	spawnItem("vase_blue", Vector2(-4000, 500))
+	spawnItem("vase_red", Vector2(-4000, 1050))
+	spawnItem("vase_green", Vector2(4500, 500))
+	spawnItem("tape", Vector2(-2350, 1050))
+	spawnItem("magazine", Vector2(4500, 800))
+#	spawnItem("magnet", Vector2(-, ))
+	spawnItem("mtg_cards", Vector2(4250, 300))
+#	spawnItem("phiranja_flower", Vector2(-, ))
+	spawnItem("radio", Vector2(-2600, 500))
+	spawnItem("spring_shoes", Vector2(-1500, 1050))
+#	spawnItem("sword", Vector2(-, ))
 
 	
 func _process(delta):
