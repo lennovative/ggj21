7d231c41454efde94e1f8057c79c9336d38e0b5c
Sascha <stu217930@mail.uni-kiel.de>
2021-01-31 17:36:19 +0100

    overhauled item generation and spawning, some fixes on quest system

--- Scripts/Level.gd
+++ Scripts/Level.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 onready var ambient = get_node("CanvasModulate")
 const ITEM_CLASS = preload("res://Scenes/Item.tscn")
+var items = []
 var objective
 onready var player = get_node("YSort/player")
-
+const POSITIONS = [Vector2(-3300, 500), Vector2(-3300, 1050), Vector2(-3300, 100), Vector2(-1250, 1050), Vector2(1250, 1050), Vector2(1500, 1050), 
+	Vector2(1775, 750), Vector2(-1500, 800), Vector2(1050, 1050), Vector2(-1250, 1025), Vector2(4250, 800), Vector2(-4900, 1050), Vector2(-1250, 1050), 
+	Vector2(-1300, 800), Vector2(4500, 1050), Vector2(4250, 1050), Vector2(4500, 800), Vector2(4250, 300), Vector2(-2600, 500), Vector2(-1500, 1050), 
+	Vector2(4500, 500), Vector2(-2350, 1050), Vector2(400, 1050), Vector2(-4000, 500), Vector2(-4000, 1050), Vector2(-4500, 500), Vector2(750, 1100),
+	Vector2(-200, 1100), Vector2(-3200, -1100), Vector2(-3200, -1150), Vector2(-3200, -1200), Vector2(-3200, -1250), Vector2(-3200, -1300), 
+	Vector2(-3200, -1350), Vector2(-3200, -1400), Vector2(-3200, -1450)]
 
 # Instances an item of given type and adds it to the scene at the given position (x, y)
 func spawnItem(type:String, position: Vector2):
@@ -17,53 +23,24 @@ func spawnItem(type:String, position: Vector2):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	spawnItem("ball_basket", Vector2(-3300, 500))
-	spawnItem("ball_volley", Vector2(-3300, 1050))
-	spawnItem("ball_white", Vector2(-3300, 100))
-	spawnItem("ball_wilson", Vector2(-3300, 200))
-	spawnItem("book_blue", Vector2(-1250, 1050))
-	spawnItem("book_red", Vector2(1250, 1050))
-	spawnItem("book_green", Vector2(1500, 1050))
-#	spawnItem("book_stack", Vector2(1500, 1050))
-#	spawnItem("bowl", Vector2(1500, 1050))
-#	spawnItem("candle_new", Vector2(1500, 1050))
-#	spawnItem("candle_old", Vector2(1500, 1050))
-	spawnItem("cat", Vector2(1775, 750))
-	spawnItem("chest", Vector2(-1500, 800))
+	print("spawning " + String(items.size()) + " items on " + String(POSITIONS.size()) + " positions")
 	spawnItem("coffee", Vector2(1050, 1050))
-	spawnItem("creeper_plush", Vector2(-1250, 1025))
-	spawnItem("doll", Vector2(4250, 800))
-	spawnItem("echolocator", Vector2(-4900, 1050))
-	spawnItem("flour", Vector2(-1250, 1050))
-	objective = spawnItem("glasses", Vector2(-1300, 800))
-	spawnItem("ice_cubes", Vector2(4500, 1050))
-	spawnItem("keys", Vector2(4250, 1050))
-	spawnItem("magazine", Vector2(4500, 800))
-#	spawnItem("magnet", Vector2(-, ))
-	spawnItem("mtg_cards", Vector2(4250, 300))
-#	spawnItem("phiranja_flower", Vector2(-, ))
-#	spawnItem("picture", Vector2(-, ))
-#	spawnItem("plant", Vector2(-, ))
-	spawnItem("radio", Vector2(-2600, 500))
-	spawnItem("spring_shoes", Vector2(-1500, 1050))
-	spawnItem("sword", Vector2(4500, 500))
-	spawnItem("tape", Vector2(-2350, 1050))
-#	spawnItem("tea_cup", Vector2(-, ))
-#	spawnItem("triforce", Vector2(-, ))
-	spawnItem("tv", Vector2(400, 1050))
-#	spawnItem("twig", Vector2(-, ))
-	spawnItem("vase_blue", Vector2(-4000, 500))
-	spawnItem("vase_red", Vector2(-4000, 1050))
-	spawnItem("vase_green", Vector2(-4500, 500))
-#	spawnItem("wrench", Vector2(-, ))
-	spawnItem("yarn", Vector2(750, 1100))
-	spawnItem("walking_aid", Vector2(-200, 1100))
+	
+	spawnItem("echolocator", Vector2(500, 1050))
+	
+	spawnItem("spring_shoes", Vector2(-500, 1050))
+	
+	for i in range (items.size()):
+		items[i].set_position(POSITIONS[min(i, POSITIONS.size()-1)])
+		get_node("YSort").add_child(items[i])
+		
 
 	
 func adjust_light(light_level: Color):
 	ambient.set_color(light_level)
 
 func start_echolocate():
+	print("locate" + String(objective.type) + " at " + String(objective.get_position()))
 	objective.echolocate()
 	get_node("Echolocate_timer").start(max(0.2, player.get_position().distance_to(objective.get_position())/1000))
 	
